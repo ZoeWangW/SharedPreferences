c4b40382240fce077c1cc7127a73483464c43e04
ZoeWangW <13438165026@163.com>
2018-09-08 22:35:47 +0800

    将数据存储
    Signed-off-by: ZoeWangW <13438165026@163.com>

---
+++ app/src/main/java/com/zoe/sharedpreferences/MainActivity.java
@@ -0,0 +1,29 @@
+package com.zoe.sharedpreferences;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button saveData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        saveData = (Button) findViewById(R.id.sava_data);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("name","Zoe");
+                editor.putInt("age",23);
+                editor.putBoolean("married",false);
+                editor.commit();
+            }
+        });
+    }
+}
