5f8777d42784f373acbb68d2f955a4091fc63bed
ZoeWangW <13438165026@163.com>
2018-09-09 19:37:03 +0800

    数据读取
    Signed-off-by: ZoeWangW <13438165026@163.com>

--- app/src/main/java/com/zoe/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/zoe/sharedpreferences/MainActivity.java
@@ -3,6 +3,7 @@ package com.zoe.sharedpreferences;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,19 +11,35 @@ public class MainActivity extends AppCompatActivity {
 
     private Button saveData;
 
+    private Button restoreData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         saveData = (Button) findViewById(R.id.sava_data);
+        restoreData = (Button) findViewById(R.id.restore_data);
         saveData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
                 editor.putString("name","Zoe");
-                editor.putInt("age",23);
-                editor.putBoolean("married",false);
+                editor.putInt("age", 23);
+                editor.putBoolean("married", false);
                 editor.commit();
+            }
+        });
+
+        restoreData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences pref = getSharedPreferences("data",MODE_PRIVATE);
+                String name = pref.getString("name", "");
+                int age = pref.getInt("age", 0);
+                boolean married = pref.getBoolean("married",false);
+                Log.d("MainActivity","name is "+ name);
+                Log.d("MainActivity","age is "+ age);
+                Log.d("MainActivity","married is "+ married);
             }
         });
     }
